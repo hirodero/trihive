6a87a79834be743b28d64533aac5adaed60419a0
hirodero <idexuta@gmail.com>
2025-05-13 04:43:54 +0700

    Fix: fix search params handling

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,8 +1,7 @@
 import { handleLogin } from "@auth0/nextjs-auth0";
 
 export function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const returnTo = searchParams.get('returnTo') || '/post-login'; 
+  const returnTo = request.nextUrl.searchParams.get("returnTo") || "/post-login";
 
   return handleLogin(request, {
     returnTo,
