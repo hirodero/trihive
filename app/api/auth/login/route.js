a59d49eec383d9f690339e1ec63bcbe40e5b528b
hirodero <idexuta@gmail.com>
2025-05-13 04:29:42 +0700

    Fix: Fix redirection issue post login

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,7 +1,10 @@
 import { handleLogin } from "@auth0/nextjs-auth0";
 
 export function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const returnTo = searchParams.get('returnTo') || '/post-login'; 
+
   return handleLogin(request, {
-    returnTo: '/post-login', 
+    returnTo,
   });
 }
