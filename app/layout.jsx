a3827723e7c2995369ce261792717528a9ebedd0
hirodero <idexuta@gmail.com>
2025-05-13 18:33:35 +0700

    Fix: fix checking path to disable footer display

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,8 @@ import { Toaster } from 'react-hot-toast';
 import { usePathname } from 'next/navigation';
 export default function RootLayout({ children }) {
   const router = usePathname();
-  const hideFooterPages = ['/trivia/games/[id]']
-  const shouldShowFooter = !hideFooterPages.includes(router);
+  const hideFooterPages = ['/trivia/games/']
+  const shouldShowFooter = !hideFooterPages.some(path => router.startsWith(path));
     return (
         <html className="scrollbar scrollbar-track-lime-700  scrollbar-thumb-lime-400" lang="en">
           <body className="flex flex-col min-h-screen -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_20%,#280_100%)]">
